039bf99ee35955f527868b409a2b38078b9765d2
abdul baruwa <abdulbaruwa@hotmail.com>
2013-06-22 20:06:26 +0100

    Basics - Prototyping ideas
    Design-Time Binding and Imported MS's WinRT UniformGrid control

--- TennisMatchScorer/MatchScoreView.xaml.cs
+++ TennisMatchScorer/MatchScoreView.xaml.cs
@@ -12,14 +12,19 @@ namespace TennisMatchScorer
     {
         public MatchScoreView()
         {
-            ViewModel = new MatchScoreViewModel();
+            //ViewModel = new MatchScoreViewModel();
+            ViewModel =  new ViewModelLocator().MatchScoreViewModel;
             this.InitializeComponent();
-
+            this.Bind(ViewModel, x => x.PointReasons, x => x.PlayerOneCommands.ItemsSource);
+            var resultofbind = this.PlayerOneCommands.ItemsSource;
 
         }
 
         public static readonly DependencyProperty ViewModelProperty =
             DependencyProperty.Register("ViewModel", typeof (MatchScoreViewModel), typeof (MatchScoreView), new PropertyMetadata(null));
+
+
+        
 
         public MatchScoreViewModel ViewModel
         {
--- TennisMatchScorer/ViewModel/MatchScoreViewModel.cs
+++ TennisMatchScorer/ViewModel/MatchScoreViewModel.cs
@@ -1,9 +1,54 @@
-﻿using ReactiveUI;
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using ReactiveUI;
 
 namespace TennisMatchScorer.ViewModel
 {
+    [DataContract]
     public class MatchScoreViewModel : ReactiveObject
     {
-             
+        public ReactiveList<PointReason>  PointReasons { get; protected set; }
+
+        public MatchScoreViewModel()
+        {
+            PointReasons = new ReactiveList<PointReason>();
+        }
+
+        //public ObservableCollection<PointReason> PointReasons
+        //{
+        //    get { return _pointReasons; }
+        //    set { this.RaiseAndSetIfChanged(ref _pointReasons, value); }
+        //}
+        
+    }
+
+    public class PointReason
+    {
+        public string Name { get; set; }
+        public string Player  { get; set; }
+    }
+
+
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled) return;
+        }
+
+        public MatchScoreViewModel MatchScoreViewModel
+        {
+            get
+            {
+                var pageVm = new MatchScoreViewModel();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    pageVm.PointReasons.Add(new PointReason(){Name = "Action " + i, Player = "Ademola"});
+                }
+           
+                return pageVm;
+            }
+        }
     }
 }
